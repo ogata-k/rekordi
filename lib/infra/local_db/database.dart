52a8a05fe88cd88710b5bf9ba54be90961287725
ogata-k <ogtkzk712@gmail.com>
2023-07-19 21:32:21 +0900

    mod: driftでログを出力するように調整

--- lib/infra/local_db/database.dart
+++ lib/infra/local_db/database.dart
@@ -216,11 +216,17 @@ class Database extends _$Database {
 /// DBと接続する
 LazyDatabase _openConnection() {
   // the LazyDatabase util lets us find the right location for the file async.
-  return LazyDatabase(() async {
-    // put the database file, called db.sqlite here, into the documents folder
-    // for your app.
-    final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(p.join(dbFolder.path, 'db.sqlite'));
-    return NativeDatabase.createInBackground(file);
-  });
+  return LazyDatabase(
+    () async {
+      // put the database file, called db.sqlite here, into the documents folder
+      // for your app.
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(p.join(dbFolder.path, 'db.sqlite'));
+      return NativeDatabase.createInBackground(
+        file,
+        logStatements: !kReleaseMode,
+      );
+    },
+    dialect: SqlDialect.sqlite,
+  );
 }
