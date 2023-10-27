c2c8f870eaff093de0893dde85d3735a30e87843
ogata-k <ogtkzk712@gmail.com>
2023-10-27 21:57:35 +0900

    mod: フォルダ構成をクリーンアーキテクチャを意識したことがわかる形に明確にした

---
+++ lib/infra/repository/db/dao/footprints.g.dart
@@ -0,0 +1,10 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'footprints.dart';
+
+// ignore_for_file: type=lint
+mixin _$FootprintsDaoMixin on DatabaseAccessor<Database> {
+  $FootprintsTable get footprints => attachedDatabase.footprints;
+
+  $AttachmentsTable get attachments => attachedDatabase.attachments;
+}
