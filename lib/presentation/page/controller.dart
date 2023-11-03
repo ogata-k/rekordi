79ff3277fe6beec3a22537c640d17fbe293704bd
ogata-k <ogtkzk712@gmail.com>
2023-11-03 17:54:02 +0900

    mod: EventBusのヘルパーをControllerに追加

--- lib/presentation/page/controller.dart
+++ lib/presentation/page/controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:rekordi/domain/component/event_bus.dart' as ceb;
 import 'package:rekordi/presentation/page/model.dart';
 
 /// ページのコントローラー
@@ -8,6 +9,10 @@ abstract class IPageController<M extends IPageModel> {
   final M _model;
 
   M get model => _model;
+
+  /// 自分の実行したアクションに関してほかのControllerにイベントを通知する。
+  /// また、ほかのControllerで通知されるイベントを監視する。
+  ceb.AppEventBus get eventBus => ceb.eventBus();
 
   void start(BuildContext context);
 
